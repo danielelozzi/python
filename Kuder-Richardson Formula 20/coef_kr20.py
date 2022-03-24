18ac1d1b92f9a94989d2d76cf92ddb6203b6ae38
Daddz <lozzi.dan@gmail.com>
2022-03-24 10:26:41 +0100

    Update coef_kr20.py

--- Kuder-Richardson Formula 20/coef_kr20.py
+++ Kuder-Richardson Formula 20/coef_kr20.py
@@ -15,21 +15,17 @@ def coef_kr20(df, columns):
     df = df.dropna()
     n = df.shape[0]
     k = len(columns)
-    list_p = []
-    list_q = []
     pq=0
     tot = []
     for j in range(k):
         pj = df[columns[j]].loc[(df[columns[j]]==1)].count()/n
         qj = df[columns[j]].loc[(df[columns[j]]==0)].count()/n
-        list_p.append(pj)
-        list_q.append(qj)
         pq+=(pj*qj)
 
     for j in range(n):
         tot_j = df.iloc[j,:].sum()
         tot.append(tot_j)
-    var = np.array(tot).var()
+    var = np.array(tot).var(ddof=1) # sample variance with DF = 1
     kr20 = (k/(k-1))*(1-(pq/var))
     
     return kr20
